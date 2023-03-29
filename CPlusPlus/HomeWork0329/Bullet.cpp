f9aacaaed8ec22f3434e257291bd303f7aa6764a
kimjinaa <cksehtk@naver.com>
2023-03-30 05:07:51 +0900

    ss

--- CPlusPlus/HomeWork0329/Bullet.cpp
+++ CPlusPlus/HomeWork0329/Bullet.cpp
@@ -1,17 +1,2 @@
-﻿#include "Bullet.h"
-#include "Player.h"
+﻿#include "Player.h"
 #include "ConsoleGameScreen.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <Windows.h>
-
-Bullet::Bullet()
-{
-
-}
-
-// ÆøÅºÀ» ¹ß»çÇÏ´Â ÇÔ¼ö
-void Bullet::FireBullet(int2 _Pos)
-{
-	BulletPos.Y -= 1;
-}
